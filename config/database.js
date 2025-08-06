c330619423ebc986cb14e2460e615de04f453f2b
Vonte <devonte.mote@gmail.com>
2025-08-06 09:56:56 -0700

    database.js commit

---
+++ config/database.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error('MongoDB connection failed:', error.message);
+    }
+};
+
+module.exports = connectDB;
